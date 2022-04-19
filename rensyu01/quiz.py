7bccb3ad5c3a21a152d6d172e0d77ba0dbc1bcb5
C0B21158 <c0b2115832@edu.teu.ac.jp>
2022-04-19 16:57:52 +0900

    試作

---
+++ rensyu01/quiz.py
@@ -0,0 +1,24 @@
+import random
+
+def shutudai():
+    qa = random.choice(list(qa_dict.items()))
+    print(qa[0])
+    return qa[1]
+
+
+def kaitou(a_list):
+    kai = input("答えを入力してください：")
+    print("入力をした答えは：",kai)
+    if kai in a_list:
+        print("正解")
+    else:
+        print("不正解です")
+
+if __name__ == "__main__":
+    qa_dict = {
+        "サザエの旦那の名前は？":["マスオ", "ますお"],
+        "カツオの妹の名前は？":["ワカメ","わかめ"],
+        "タラオはカツオから見てどんな関係？":["おい","甥","甥っ子","おいっこ"]
+    }
+a_list = shutudai()
+kaito = a_list
