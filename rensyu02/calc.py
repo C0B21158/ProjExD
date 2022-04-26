37f1e91adc4e8634958bb5f68350a410ea92805a
C0B21158 <c0b2115832@edu.teu.ac.jp>
2022-04-26 17:41:18 +0900

    トリマ完成

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -1,11 +1,13 @@
-from pynput import keyboard
 import tkinter as tk
 import tkinter.messagebox as tkm
+
+jj = 0
 def button_click(event):
     button=event.widget
     num = button["text"]
     #tkm.showinfo(txt, f"{num}のボタンがクリックされました")
-    entry.insert(tk.END, num )
+    entry.insert(tk.END, num)
+    button["background"] = "red"
 
 def eq_click(event):
     get = entry.get()
@@ -13,10 +15,15 @@ def eq_click(event):
     entry.delete(0, tk.END)
     entry.insert(tk.END, ans)
 
+def ac_click(a):
+    entry.delete(0, tk.END)
+ 
+
 if __name__ == "__main__":
     r,c = 1,1
+    cl = "white"
     root = tk.Tk()
-    root.title("tk")
+    root.title("電卓")
     root.geometry("400x500")
     entry = tk.Entry(root , 
                      justify = "right",
@@ -24,27 +31,10 @@ if __name__ == "__main__":
                      font=("Times New Roman",40)
                      )
     entry.grid(row=0, column=0, columnspan=4)
+
     
-    if keyboard.is_pressed("q"):
-            for i, num in enumerate(range(9,-1,-1), 1):
-                button = tk.Button(root, text=num, font=("Times New Roman",30), background="blue")
-                button.bind("<1>", button_click)
-                button.grid(row = r, column= c, padx = 10, pady = 10)
-                if (i)%3 == 0:
-                    r += 1
-                    c = 0
-                c += 1
-                button = tk.Button(root, text = "+",font=("Times New Roman",30))
-                button.bind("<1>", button_click)
-                button.grid(row = r, column= c, padx = 10, pady = 10)
-        
-                button = tk.Button(root, text = "=",font=("Times New Roman",30))
-                button.bind("<1>", eq_click)
-                button.grid(row = 4, column= 4, padx = 10, pady = 10)
-
-
     for i, num in enumerate(range(9,-1,-1), 1):
-        button = tk.Button(root, text=num, font=("Times New Roman",30))
+        button = tk.Button(root, text=num, font=("Times New Roman",30) ,background = cl)
         button.bind("<1>", button_click)
         button.grid(row = r, column= c, padx = 10, pady = 10)
         if (i)%3 == 0:
@@ -55,10 +45,17 @@ if __name__ == "__main__":
         button.bind("<1>", button_click)
         button.grid(row = r, column= c, padx = 10, pady = 10)
         
+        button = tk.Button(root, text = "-",font=("Times New Roman",30))
+        button.bind("<1>", button_click)
+        button.grid(row = 4, column= 3, padx = 10, pady = 10)
+
         button = tk.Button(root, text = "=",font=("Times New Roman",30))
         button.bind("<1>", eq_click)
         button.grid(row = 4, column= 4, padx = 10, pady = 10)
     
+        button = tk.Button(root, text = "AC",font=("Times New Roman",25))
+        button.bind("<1>", ac_click)
+        button.grid(row = 3, column= 4, padx = 10, pady = 10)
     
 
     root.mainloop()
