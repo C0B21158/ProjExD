19712f43d6ce8e5c51eae019b735ce511261e001
C0B21158 <c0b2115832@edu.teu.ac.jp>
2022-04-26 14:35:05 +0900

    ボタン作成

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -1,11 +1,22 @@
+from ast import Num
 import tkinter as tk
 import tkinter.messagebox as tkm
 
 if __name__ == "__main__":
-
+    r,c = 0,0
     root = tk.Tk()
     root.title("tk")
     root.geometry("300x450")
-    button = tk.Button(root, ext="1", font ="Times New Roman"30)
-    button.pack()
+    for i, num in renge(9,-1,-1):
+        button = tk.Button(root, text=num)
+        button.grid(row = r, column= c)
+        if (i+1)%3 == 0:
+            r += 1
+            c = 0
+        c +=1
+        
+        button.pack()
+    
+    
+    
     root.mainloop()
