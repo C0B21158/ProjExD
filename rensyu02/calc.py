a1fb4d1d019a729fdee935e203267bba54c75928
C0B21158 <c0b2115832@edu.teu.ac.jp>
2022-04-26 14:40:25 +0900

    ボタン作成

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -7,14 +7,15 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("tk")
     root.geometry("300x450")
-    for i, num in renge(9,-1,-1):
-        button = tk.Button(root, text=num)
-        button.grid(row = r, column= c)
+    for i, num in enumerate(range(9,-1,-1), 1):
+        button = tk.Button(root, text=num, font=("Times New Roman",30))
+        button.grid(row = r, column= c, padx = 10, pady = 10)
         if (i+1)%3 == 0:
             r += 1
             c = 0
         c +=1
         
+
         button.pack()
     
     
