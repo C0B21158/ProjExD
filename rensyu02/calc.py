a794dc8645e28042471b3526c2f05d7bbfb896e4
C0B21158 <c0b2115832@edu.teu.ac.jp>
2022-04-26 15:21:31 +0900

    結合

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -1,19 +1,27 @@
 
 import tkinter as tk
 import tkinter.messagebox as tkm
+from turtle import right
 def button_click(event):
-    btn=event.widget
-    txt = btn["text"]
+    button=event.widget
+    txt = button["text"]
     tkm.showinfo(txt, f"{num}のボタンがクリックされました")
 
 if __name__ == "__main__":
-    r,c = 0,1
+    r,c = 1,1
     root = tk.Tk()
     root.title("tk")
     root.geometry("300x450")
+    entry = tk.Entry(root , 
+                     justify = "right",
+                     width = 10,
+                     font=("Times New Roman",40)
+                     )
+    entry.grid(row=0, column=0, clumnspan=0)
+
     for i, num in enumerate(range(9,-1,-1), 1):
         button = tk.Button(root, text=num, font=("Times New Roman",30))
-        btn.bind("<1>", button_click)
+        button.bind("<1>", button_click)
         button.grid(row = r, column= c, padx = 10, pady = 10)
         if (i)%3 == 0:
             r += 1
