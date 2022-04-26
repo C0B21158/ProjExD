d36564cfadbdc1e63a882dbb6b30f8176fa0c456
C0B21158 <c0b2115832@edu.teu.ac.jp>
2022-04-26 14:23:10 +0900

    ウィンドウ作成

---
+++ rensyu02/calc.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+if __name__ == "__main__":
+
+    root = tk.Tk()
+    root.title("tk")
+    root.geometry("300x450")
+    button = tk.Button(root, ext="1", font ="Times New Roman"30)
+    button.pack()
+    root.mainloop()
