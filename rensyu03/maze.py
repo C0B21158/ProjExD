daef5f82c3a7a1ce5578efe94d372130d9b69ca2
C0B21158 <c0b2115832@edu.teu.ac.jp>
2022-05-10 17:53:14 +0900

    消しましたよ#6

--- rensyu03/maze.py
+++ rensyu03/maze.py
@@ -38,7 +38,6 @@ def main_proc():
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
-    root.geometry("1500x900")
 
     canvas = tk.Canvas(root,width = "1500", height = "900" ,bg = "black")
     canvas.pack()
