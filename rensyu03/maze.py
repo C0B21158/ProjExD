76fb9107bafc85af2a0621d798e75f4fb34a711b
C0B21158 <c0b2115832@edu.teu.ac.jp>
2022-05-10 17:05:42 +0900

    ゴールとスタート追加

--- rensyu03/maze.py
+++ rensyu03/maze.py
@@ -1,4 +1,7 @@
+
+import maze_maker
 import tkinter as tk
+import tkinter.messagebox as tkm
 
 def key_down(event):
     global key
@@ -8,6 +11,29 @@ def key_up(event):
     global key
     key = ""
 
+def goal():
+    tkm.showinfo("無","終わったんだｙ")
+    root.destroy()
+
+
+    
+def main_proc():
+    global cx ,cy , key , mx, my
+    if key == "Up" and meiro[my-1][mx]==0:
+        my -= 1
+    if key == "Down" and meiro[my+1][mx]==0:
+        my += 1
+    if key == "Left" and meiro[my][mx-1]==0:
+        mx -= 1
+    if key == "Right" and meiro[my][mx+1]==0:
+        mx += 1
+    cx = mx*100
+    cy = my*100
+    
+    canvas.coords("tori", cx+50, cy+50)
+    if mx == 13 and my == 7:
+        goal()
+    root.after(100, main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -16,19 +42,29 @@ if __name__ == "__main__":
 
     canvas = tk.Canvas(root,width = "1500", height = "900" ,bg = "black")
     canvas.pack()
-
-    tori = tk.PhotoImage(file="c:/Users/admin/Documents/ProjExD_pub/fig/4.png")
-    cx ,cy = 300 ,400
-    canvas.create_image(cx, cy, image =tori, tag = "tori")
     
+
+
     key = ""
     root.bind("<KeyPress>", key_down)
-
     root.bind("<KeyRelease>", key_up)
     
+    meiro = maze_maker.make_maze(15,9)
+    maze_maker.show_maze(canvas, meiro)
+    canvas.create_rectangle(100, 100, 200, 200, fill="Midnight Blue")
+    canvas.create_rectangle(1300, 700, 1400, 800, fill="firebrick3")
+
+    mx, my = 1,1
+    cx = mx*100
+    cy = my*100
+
+    
+    tori = tk.PhotoImage(file="c:/Users/admin/Documents/ProjExD_pub/fig/4.png")
+    
+ 
+    canvas.create_image(cx, cy, image =tori, tag = "tori")
+
+
+    main_proc()
     root.mainloop()
-
-
-
-
 
