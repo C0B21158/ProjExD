7ebf5f3c1cc602b57887ad5a7d1238c15e642e8d
C0B21158 <c0b2115832@edu.teu.ac.jp>
2022-05-17 15:28:32 +0900

    #練習６

--- rensyu04/dodge_bomb.py
+++ rensyu04/dodge_bomb.py
@@ -27,10 +27,11 @@ def main():
 
     #練習５
     bomb = pg.Surface((20,20))     #爆弾用Surface 
+    bomb.set_colorkey((0,0,0))
     pg.draw.circle(bomb, (255,0, 0), (10,10),10)
     bomb_rect = bomb.get_rect()
     bomb_rect.centerx = random.randint(0,sc_rect.width)
-    bomb_rect_centery = random.randint(0,sc_rect.height)
+    bomb_rect.centery = random.randint(0,sc_rect.height)
     screen.blit(bomb, bomb_rect)    #爆弾用のSurfaceを画面用に貼り付け
 
     while True:
@@ -45,6 +46,11 @@ def main():
                 tori_rect.centerx += delta[0]
                 tori_rect.centery += delta[1]
         screen.blit(tori_img, tori_rect)
+
+        #練習６
+        vx, vy = +1, +1
+        bomb_rect.move_ip(vx,vy)
+        screen.blit(bomb,bomb_rect)
 
         pg.display.update()
         clock.tick(1000)
